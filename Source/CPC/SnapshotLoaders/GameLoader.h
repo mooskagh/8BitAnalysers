c64238ae3ba1f8f1c47605816e1decf1bcaa4830
Colourclash <sam@colourclash.co.uk>
2023-04-11 21:57:32 +0100

    Add caching to cpc game loader

--- Source/CPC/SnapshotLoaders/GameLoader.h
+++ Source/CPC/SnapshotLoaders/GameLoader.h
@@ -5,11 +5,24 @@ class FCpcEmu;
 class FCpcGameLoader : public IGameLoader
 {
 public:
+	// IGameLoader
+	bool LoadGame(const char* pFileName) override;
+	ESnapshotType GetSnapshotTypeFromFileName(const std::string& fn) override;
+	// ~IGameLoader
+
 	void Init(FCpcEmu* pCpc)
 	{
 		pCpcEmu = pCpc;
 	}
-	bool LoadGame(const char* pFileName) override;
-	ESnapshotType GetSnapshotTypeFromFileName(const std::string& fn) override;
+
+	// When caching is enabled, repeated loading of the same snapshot will not need to load the file from disk.
+	void SetCachingEnabled(bool bEnabled);
+	void ClearCache();
+
 	FCpcEmu* pCpcEmu = 0;
+
+	bool bCachingEnabled = false;
+	uint8_t* pDataCache = 0;
+	size_t CachedDataSize = 0;
+	std::string CachedFilename;
 };
